6cccd7f880ee6021d65fd3ea5bc3a45411dae319
agirbau <andreu.girbau@gmail.com>
2023-10-20 13:29:45 +0900

    Add run script.

---
+++ esim_torch/scripts/run_all.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Paths of python interpreters
+vid2e_path="/home/agirbau/work/event_cameras/rpg_vid2e"
+evVision_path="/home/agirbau/work/event_cameras/eventVision"
+python_vid2e="$vid2e_path/venv/bin/python"
+python_evVision="$evVision_path/venv/bin/python"
+
+# Video data to convert to events
+data_path=""
+
+# Extract frames from video
+
+# Generate slow motion gray-scale frames
+"$python_vid2e"
+
+# Generate events from slow motion video
+# python...
+
+# Render video of event data
+# python...
